caa04ae0b79e453d265a66f4564d8201d89ca083
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-09-02 13:30:03 +0600

    third

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const searchBook = () => {
     const searchText = inputField.value;
     //clear inputField
     inputField.value = '';
-    const url = ` http://openlibrary.org/search.json?q=${searchText}`
+    const url = ` https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayBook(data.docs))
@@ -21,7 +21,7 @@ const displayBook = (docs) => {
         console.log(doc)
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = `<div class="card" style = "height: 600px; width: 450px margin: 5px">
+        div.innerHTML = `<div class="card" style = "height: 600px; width: 450px margin: 5px; padding:5px">
         <img src=" https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg" class="card-img-top" alt="">
         <div class="card-body">
           <h5 class="card-title">${doc.title}</h5>
@@ -32,10 +32,5 @@ const displayBook = (docs) => {
       </div>`
       booksContainer.appendChild(div)
       resutlFound.innerHTML = `<p class ="text-white fw-bold text-center">${docs.length} result found</p>`
-
-
-
-
-
     })
 }
